2c47aa1f16225b1e010d9543c71bf41c264a5e07
Pavel Vasin <rat4vier@gmail.com>
2012-07-24 16:52:32 +0400

    use Paginator

--- src/main/scala/badcode/snippet/Browse.scala
+++ src/main/scala/badcode/snippet/Browse.scala
@@ -4,14 +4,18 @@ import xml.NodeSeq
 import badcode.model.BadCode
 import net.liftweb.util.Helpers
 import Helpers._
+import net.liftweb.http.PaginatorSnippet
+import net.liftweb.mapper.{MaxRows, StartAt}
 
-class Browse {
-  def howdy(in: NodeSeq): NodeSeq = {
-    val codes = BadCode.findAll()
-    codes.flatMap(code => Helpers.bind("entry", in,
+class Browse extends PaginatorSnippet[BadCode] {
+  override def itemsPerPage = 20
+  override def count = BadCode.count
+  override def page = BadCode.findAll(StartAt(curPage * itemsPerPage), MaxRows(itemsPerPage))
+
+  def renderPage(in: NodeSeq): NodeSeq =
+    page.flatMap(code => Helpers.bind("entry", in,
       "content" -> code.content,
       "description" -> code.description,
       "author" -> code.author,
       "date" -> code.createdAt))
-  }
 }
