6dee2e8ab8957add9462eef6c92f7b2e0bf2b417
Pavel Vasin <rat4vier@gmail.com>
2012-07-28 16:25:31 +0400

    browse: order by date

--- src/main/scala/wtfcode/snippet/Browse.scala
+++ src/main/scala/wtfcode/snippet/Browse.scala
@@ -3,13 +3,16 @@ package wtfcode.snippet
 import xml.NodeSeq
 import wtfcode.model.Post
 import net.liftweb.http.PaginatorSnippet
-import net.liftweb.mapper.{MaxRows, StartAt}
+import net.liftweb.mapper.{Descending, OrderBy, MaxRows, StartAt}
 import wtfcode.util.CodeBinder
 
 class Browse extends PaginatorSnippet[Post] {
   override def itemsPerPage = 20
   override def count = Post.count
-  override def page = Post.findAll(StartAt(curPage * itemsPerPage), MaxRows(itemsPerPage))
+  override def page = Post.findAll(
+    OrderBy(Post.createdAt, Descending),
+    StartAt(curPage * itemsPerPage),
+    MaxRows(itemsPerPage))
 
   def renderPage(in: NodeSeq): NodeSeq =
     page.flatMap(code => CodeBinder(in, code))
