fab62f7411241ef37e8cf48e6a3b4e28e49fd1eb
Pavel Vasin <rat4vier@gmail.com>
2012-08-07 12:56:57 +0400

    saveip: in production mode use X-Forwarded-For

--- src/main/scala/wtfcode/model/SaveIP.scala
+++ src/main/scala/wtfcode/model/SaveIP.scala
@@ -2,6 +2,7 @@ package wtfcode.model
 
 import net.liftweb.http.S
 import net.liftweb.mapper.{MappedText, BaseMapper}
+import net.liftweb.util.Props
 
 trait SaveIP {
   self: BaseMapper =>
@@ -9,6 +10,10 @@ trait SaveIP {
   val ipAddress: MappedText[MapperType] = new MyIPAddress(this)
 
   protected class MyIPAddress(obj: self.type) extends MappedText[MapperType](obj.asInstanceOf[MapperType]) {
-    override def defaultValue = S.containerRequest.map(_.remoteAddress).openOr("NULL")
+    override def defaultValue =
+      Props.productionMode match {
+        case true => S.request.flatMap(_.header("X-Forwarded-For")).openOr("NULL")
+        case false => S.containerRequest.map(_.remoteAddress).openOr("NULL")
+      }
   }
 }
