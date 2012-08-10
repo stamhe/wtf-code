f11990bf9c2b0f87e9b10203a66388ace57e4dd4
Pavel Vasin <rat4vier@gmail.com>
2012-08-10 16:28:53 +0400

    mainpage: improve a bit

--- src/main/scala/wtfcode/snippet/MainPage.scala
+++ src/main/scala/wtfcode/snippet/MainPage.scala
@@ -6,12 +6,8 @@ import wtfcode.model._
 
 class MainPage {
   def howdy = {
-    ".time" #> (new _root_.java.util.Date).toString &
-    ".codes" #> Post.count &
     ".users" #> User.count &
-    ".comments" #> Comment.count &
-    ".bookmarks" #> Bookmark.count &
-    ".last_seen" #> LastSeen.count &
-    ".notifications" #> Notification.count
+    ".posts" #> Post.count &
+    ".comments" #> Comment.count
   }
 }
