721b9f27ddb6f8bda49942f201de71569c9c5644
Pavel Vasin <rat4vier@gmail.com>
2012-08-31 20:40:03 +0400

    Notifications: highlight unread

--- src/main/scala/wtfcode/snippet/Notifications.scala
+++ src/main/scala/wtfcode/snippet/Notifications.scala
@@ -3,7 +3,6 @@ package wtfcode.snippet
 import xml.NodeSeq
 import wtfcode.model.{User, Notification}
 import net.liftweb.util.Helpers._
-import net.liftweb.http.PaginatorSnippet
 
 class Notifications extends BootstrapPaginatorSnippet[Notification] {
   val startAt = curPage * itemsPerPage
@@ -16,9 +15,13 @@ class Notifications extends BootstrapPaginatorSnippet[Notification] {
     ".notifications *" #> ((in: NodeSeq) => page.flatMap {notification => bindNotification(notification)(in)})
 
   def bindNotification(notification: Notification): (NodeSeq => NodeSeq) = {
+    ".read [class]" #> read(notification) &
     ".link [href]" #> notification.link &
     ".from *" #> notification.from.map(_.nickName.get).openOr("Guest") &
     ".from [href]" #> notification.from.map(_.link).openOr("#") &
     ".date *" #> notification.createdAt
   }
+
+  private def read(notification: Notification): Option[String] =
+    if (!notification.read) Some("unseen") else None
 }
