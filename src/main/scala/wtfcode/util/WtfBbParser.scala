803e3eb885731905ce2c74325d06324feeb5333e
Pavel Vasin <rat4vier@gmail.com>
2012-09-11 22:13:57 +0400

    use icon in mentions

--- src/main/scala/wtfcode/util/WtfBbParser.scala
+++ src/main/scala/wtfcode/util/WtfBbParser.scala
@@ -9,7 +9,7 @@ object WtfBbParser extends ExtendableBbParser {
 
   case class UserTag(content: String) extends BbAst {
     override val toHtml = User.findByNickName(content) match {
-      case Full(user) => <a href={user.link}>{"@" + user.nickName}</a>
+      case Full(user) => <a href={user.link}><i class="icon-user"/>{user.nickName}</a>
       case _ => Text("[user]" + content + "[/user]")
     }
   }
